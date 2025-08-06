35d8e7e1c3ba52ffe1571bb3013d0f5a43ece911
W2-87359-Manasi Deshpande <manasideshpande1994@gmail.com>
2025-08-06 17:52:51 +0530

    Day 3 task

---
+++ day3/Book.java
@@ -0,0 +1,68 @@
+package day3;
+
+public class Book {
+    private int serialNo;
+    private String name;
+    private String author;
+    private int quantity;
+    private int availableQuantity;
+
+    public Book( String name, String author, int quantity) {
+        //this.serialNo = serialNo;
+        this.name = name;
+        this.author = author;
+        this.quantity = quantity;
+        //this.availableQuantity = availableQuantity;
+    }
+
+    public int getSerialNo() {
+        return serialNo;
+    }
+
+    public void setSerialNo(int serialNo) {
+        this.serialNo = serialNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getAvailableQuantity() {
+        return availableQuantity;
+    }
+
+    public void setAvailableQuantity(int availableQuantity) {
+        this.availableQuantity = availableQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "serialNo=" + serialNo +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", quantity=" + quantity +
+                ", availableQuantity=" + availableQuantity +
+                '}';
+    }
+}
